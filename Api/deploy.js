ddf0bae7a6bc824281d4c35e9b2f34439aa6fdc9
erlanggaputra9329-debug <erlangga.putra9329@smp.belajar.id>
2026-05-26 20:22:02 +0700

    Create deploy.js

---
+++ Api/deploy.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  res.status(200).json({ message: 'OK', tokenExists: !!process.env.VERCEL_API_TOKEN });
+}
